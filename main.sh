c5de7e37ce5476a82007f041d0cf8af3ab2b0d6b
Ginfung <ginfungchan@gmail.com>
2018-05-05 00:20:32 -0400

    add mpi deployment

---
+++ main.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd /home/jchen37/actL_re
+git pull
+mkdir out
+mkdir err
+rm out/*
+rm err/*
+for i in p3a p3b p3c;
+do
+for k in nsgaii riot;
+do
+	sbatch -p opteron main.mpi $i $k
+	sbatch -p opteron main.mpi $i $k
+done;
+done;
